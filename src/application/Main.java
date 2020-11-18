2f8976c4e875669d9e7e0437981c11032b45d443
Jina <Jina@Owner-PC.lan1>
2020-11-18 13:45:40 -0600

    from Jina

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import application.controller.MenuController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,15 +7,17 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+// test from Jina
+
 /**
 * The Main class launches the application and sets up MainController.fxml.
-* 
+*
 *  @author Gabrielle Albrecht/ypo253
 *  @author Dariel Malave Perez
 *  @author Robert Taylor
 *  @author Jina Wilde
 *  @author Zane Zaiontz
-*  
+*
 *  UTSA CS 3443 - Team Project
 *  Fall 2020
 */
@@ -25,22 +27,22 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("view/Menu.fxml"));
-			Parent root = loader.load(); 
-			primaryStage.setTitle("Weather Watch"); 
-			
+			Parent root = loader.load();
+			primaryStage.setTitle("Weather Watch");
+
 			MenuController mC = loader.getController();
 			mC.initialize();
-			
+
 			Scene scene = new Scene(root, 800,800);
 			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
+
 		} catch(Exception e) {
 			//e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -61,17 +63,17 @@ public class Main extends Application {
 //		Map<String, Object> map = new Gson().fromJson(
 //				str, new TypeToken<HashMap<String, Object>>() {}.getType()
 //				);
-//		
+//
 //		return map;
 //	}
-//	
+//
 //	public static void main(String[] args) {
 //		try {
 //			StringBuilder result = new StringBuilder();
 //			// Example URL for San Francisco (works for cities)
 //			URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=48fb3ffd07fec36d9f2a24f46772bada&units=imperial");
 //			URLConnection conn = url.openConnection();
-//			
+//
 //			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 //			String line;
 //			while ((line = rd.readLine()) != null) {
@@ -79,11 +81,11 @@ public class Main extends Application {
 //			}
 //			rd.close();
 //			System.out.println(result);
-//			
+//
 //			Map<String, Object> respMap = jsonToMap(result.toString());
 //			Map<String, Object> mainMap = jsonToMap(respMap.get("main").toString());
 //			Map<String, Object> windMap = jsonToMap(respMap.get("wind").toString());
-//			
+//
 //			System.out.println("Current Temperatures: " + mainMap.get("temp"));
 //			System.out.println("Current Humidity: " + mainMap.get("humidity"));
 //			System.out.println("Wind speeds: " + windMap.get("speed"));
