76fae6c20995fdf85417967f45f99827bcc9b5bc
GabrielleAlbrecht <72949291+GabrielleAlbrecht@users.noreply.github.com>
2020-11-22 14:36:38 -0600

    Update MenuController.java

--- src/application/controller/MenuController.java
+++ src/application/controller/MenuController.java
@@ -50,7 +50,7 @@ public class MenuController {
 	@FXML
 	private void handleButton2Action(ActionEvent event){
 		try{
-			URL fileUrl = Main.class.getResource("/application/view/DetailedWeather.fxml");
+			URL fileUrl = Main.class.getResource("/application/view/Radar.fxml");
 			new FXMLLoader();
 			view = FXMLLoader.load(fileUrl);
 			mainPane.setCenter(view);
