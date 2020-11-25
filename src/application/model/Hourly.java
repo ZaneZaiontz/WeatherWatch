a2431dca9e446c636ce81ff4322888a05d0478c4
GabrielleAlbrecht <72949291+GabrielleAlbrecht@users.noreply.github.com>
2020-11-24 18:38:59 -0600

    Add the base for Javadoc comments

--- src/application/model/Hourly.java
+++ src/application/model/Hourly.java
@@ -3,74 +3,146 @@ package application.model;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * 
+ * @author Gabrielle Albrecht/ypo253
+ *
+ */
+
 public class Hourly {
 	private String temp,  humidity, windSpeed, description, icon;
 	Long date;
 
-//~~ Constructor
-public Hourly(String temp, String humidity, String windSpeed, String description, String icon, Long date){
-	this.temp = temp;
-	this.humidity = humidity;
-	this.windSpeed = windSpeed;
-	this.description = description;
-	this.icon = icon;
-	this.date = date;
-} 
+	//~~ Constructor
+	/**
+	 * 
+	 * @param temp
+	 * @param humidity
+	 * @param windSpeed
+	 * @param description
+	 * @param icon
+	 * @param date
+	 */
+	public Hourly(String temp, String humidity, String windSpeed, String description, String icon, Long date){
+		this.temp = temp;
+		this.humidity = humidity;
+		this.windSpeed = windSpeed;
+		this.description = description;
+		this.icon = icon;
+		this.date = date;
+	} 
+	
+	//~~ Setters
+	/**
+	 * 
+	 * @param data
+	 */
+	public void setTemp(String data){
+		this.temp = data;
+	}
+	
+	/**
+	 * 
+	 * @param data
+	 */
+	public void setWindSpeed(String data){
+		this.windSpeed = data;
+	}
+	/**
+	 * 
+	 * @param data
+	 */
+	public void setHumidity(String data){
+		this.humidity = data;
+	}
+	/**
+	 * 
+	 * @param data
+	 */
+	public void setIcon(String data){
+		this.icon = data;
+	}
+	/**
+	 * 
+	 * @param data
+	 */
+	public void setDescription(String data){
+		this.description = data;
+	}
+	/**
+	 * 
+	 * @param date
+	 */
+	public void setDate(Long date){
+		this.date = date;
+	}
+	
+	//~~ Getters
+	/**
+	 * 
+	 * @return
+	 */
+	public String getTemp(){
+		return this.temp;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getWindSpeed(){
+		return this.windSpeed;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getHumidity(){
+		return this.humidity;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getIcon(){
+		return this.icon;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getDescription(){
+		return this.description;
+	}
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public Long getDate(){
+		return this.date;
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public String getDay(){
+		 Date date = new Date(this.date*1000L);
+		 SimpleDateFormat jdf = new SimpleDateFormat("EEE MM dd hh:mm:ss z");
+		 String newDate = jdf.format(date);
 
-//~~ Setters
-public void setTemp(String data){
-	this.temp = data;
-}
-
-public void setWindSpeed(String data){
-	this.windSpeed = data;
-}
-public void setHumidity(String data){
-	this.humidity = data;
-}
-public void setIcon(String data){
-	this.icon = data;
-}
-public void setDescription(String data){
-	this.description = data;
-}
-public void setDate(Long date){
-	this.date = date;
-}
-
-//~~ Getters
-public String getTemp(){
-	return this.temp;
-}
-public String getWindSpeed(){
-	return this.windSpeed;
-}
-public String getHumidity(){
-	return this.humidity;
-}
-public String getIcon(){
-	return this.icon;
-}
-public String getDescription(){
-	return this.description;
-}
-public Long getDate(){
-	return this.date;
-}
-
-public String getDay(){
-	 Date date = new Date(this.date*1000L);
-//	 return date.toString();
-//	 SimpleDateFormat jdf = new SimpleDateFormat("E dd");
-	 SimpleDateFormat jdf = new SimpleDateFormat("EEE MM dd hh:mm:ss z");
-	 String newDate = jdf.format(date);
-//	 String java_date = jdf.format(date);
-	 return newDate;
-}
-
-//~~ toString()
-public String toString(){
-	//return (this.temp + "Humidity: " + this.humidity + " " + this.windSpeed + " " + this.date + " " + getDay());
-	return (getDay() + ": " + this.temp + "°F, " + this.humidity + "% humidity, " + this.windSpeed + " mph wind");
-}
+		 return newDate;
+	}
+	
+	//~~ toString()
+	/**
+	 * 
+	 */
+	public String toString(){
+		return (getDay() + ": " + this.temp + "Â°F, " + this.humidity + "% humidity, " + this.windSpeed + " mph wind");
+	}
 }
